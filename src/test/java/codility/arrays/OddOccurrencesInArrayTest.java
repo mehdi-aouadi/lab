8662caf4ef883422309f2bcff469dcfe4df22868
mehdi-aouadi <mehdi.aouadi@gmail.com>
2020-03-27 11:53:25 +0100

    [CODILITY] Odd Occurrence in Array

---
+++ src/test/java/codility/arrays/OddOccurrencesInArrayTest.java
@@ -0,0 +1,18 @@
+package codility.arrays;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class OddOccurrencesInArrayTest {
+
+  @Test
+  public void oddOccurrencesInArrayTest() {
+    Assert.assertEquals(7, OddOccurrencesInArray.oddOccurrencesInArray(new int[] {9, 3, 9, 3, 9, 7, 9}));
+  }
+
+  @Test
+  public void oddOccurrencesWithSortTest() {
+    Assert.assertEquals(7, OddOccurrencesInArray.oddOccurrenceWithSort(new int[] {9, 3, 9, 3, 9, 7, 9}));
+  }
+
+}
